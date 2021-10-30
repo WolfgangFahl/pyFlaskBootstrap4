1a2a13c29991557f093a18f1ef3b48d5c447268e
Wolfgang Fahl <wf@bitplan.com>
2021-10-30 14:16:41 +0200

    improves basedUrl

--- fb4/app.py
+++ fb4/app.py
@@ -68,16 +68,22 @@ class AppWrap:
         jinjaLoader=jenv.app.jinja_loader
         jinjaLoader.searchpath.append(path)
         
-    def basedUrl(self,url):
+    def basedUrl(self,url:str)->str:
         '''
         add the base url if need be
+        
+        Args:
+            url(str):  the url to base
+        
+        Return:
+            str: the completed url
         ''' 
         if self.baseUrl:
             baseUrl=self.baseUrl
         else:
-            baseUrl="http://%s:%s" % (self.host,self.port)
+            baseUrl=f"http://{self.host}:{self.port}" 
         if url.startswith("/"):
-            url="%s%s" % (baseUrl,url)
+            url=f"{baseUrl}{url}"
         return url
         
     @staticmethod
