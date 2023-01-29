00ce9785418f9bfb83009a649dad15a77e27b80e
Wolfgang Fahl <wf@bitplan.com>
2023-01-29 15:32:37 +0100

    upgrades to fix https://stackoverflow.com/questions/75275563/attributeerror-module-sqlalchemy-has-no-attribute-all-in-github-actions/75275623#75275623

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except:
     long_description = open('../../README.md').read()
 
 setup(name='pyFlaskBootstrap4',
-      version='0.5.1',
+      version='0.6.0',
       description='python flask + Bootstrap 4 static components and templates for webprojects',
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -30,28 +30,28 @@ setup(name='pyFlaskBootstrap4',
       ),
       classifiers=[
             'Programming Language :: Python',
-            'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10'
+            'Programming Language :: Python :: 3.11'
       ],
       packages=['fb4'],
       package_data={
           'fb4': ['templates/sse/*.html','templates/fb4common/*.html'],
       },
       install_requires=[
-          'Bootstrap-Flask~=1.8.0',
-          'Flask-Dropzone~=1.6.0',	
-          'Flask~=2.0.2',
-          'Flask-HTTPAuth~=4.5.0',
-          'Flask-Login~=0.6.2',
-          'Flask-SQLAlchemy~=2.5.1',
-          'Flask-WTF~=1.0.0',
-          'WTForms~=3.0.0',
+          'Bootstrap-Flask>=1.8.0',
+          'Flask-Dropzone>=1.6.0',	
+          'Flask>=2.0.2',
+          'Flask-HTTPAuth>=4.5.0',
+          'Flask-Login>=0.6.2',
+          'Flask-SQLAlchemy>=3.0.2',
+          'Flask-WTF>=1.0.0',
+          'WTForms>=3.0.0',
           'SQLAlchemy-Utils',
           'requests',
           'pydevd',
           'pyDispatcher',
-          'APScheduler~=3.9.1'
+          'APScheduler>=3.9.1'
       ],
       zip_safe=False)
